99ae5b6b0c3bf2dc5e1cdffc81a6f645d58a23ce
Adrian Berisha <adrianberisha@MBP-von-Adrian.telekom.ip>
2024-11-16 00:17:13 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,49 @@
+const passwordString = 'abcdefghijklmnopqrstuvwxyzßABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+let newPass = '';
+const btn = document.getElementById("btn");
+const input = document.getElementById("input");
+const copy = document.getElementById("copy");
+
+function generatePassword() {
+    let length = Number(document.getElementById("length").value);
+    if (length == 0) {
+        length = 10;
+    }
+    for (let i = 0; i < length; i++) {
+        const index = Math.floor(Math.random() * passwordString.length);
+        newPass += passwordString[index];
+    }
+}
+
+function showPassword() {
+    input.value = newPass;
+}
+
+async function copyToClipboard(text) {
+    try {
+      await navigator.clipboard.writeText(text);
+      if (input.value == ""){
+        return;
+      } else {
+        document.getElementById("copyText").innerHTML = "<p>Copied to Clipboard</p>"
+        setTimeout(() => {
+            document.getElementById("copyText").innerHTML = "";
+        }, 3000);
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+}
+
+copy.addEventListener("click", () => {
+    copyToClipboard(input.value);
+})
+
+btn.addEventListener("click", () => {
+    newPass = '';
+    generatePassword();
+    showPassword();
+})
+
+generatePassword();
+console.log(newPass);
